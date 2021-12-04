e2b61cdb246549f76878ef1b5da1410382689baa
Joonatan Korpela <joonatan.aatos@gmail.com>
2021-12-04 17:32:24 +0200

    Find all links for cars

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,8 +7,10 @@ import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Main {
 
@@ -19,6 +21,7 @@ public class Main {
         public int range;
         public ArrayList<String> supportedChargers;
         public int maxChargingPower;
+        public String link;
     }
 
     public static void main(String[] args) throws Exception {
@@ -47,23 +50,63 @@ public class Main {
                     String name =
                             data[0].replaceAll(String.valueOf((char) 160), "") + "_" +
                             data[1].replaceAll(String.valueOf((char) 160), "");
-                    CarData car = new CarData();
-                    car.name = name;
-                    car.count = count;
-                    cars.add(car);
+                    name = name.replaceAll("-", "_").replaceAll(" ", "_").replaceAll("!", "1").replaceAll("BMWI", "BMW").replaceAll("BENZ_", "").replaceAll("TESLAMOTORS", "TESLA").replaceAll("MODEL", "MODEL_").replaceAll("EQC400", "EQC").replaceAll("PEUGEOT_", "PEUGEOT_E_").replaceAll("VOLKSWAGEN_", "VOLKSWAGEN_E_");
+                    if (name.equals("VOLKSWAGEN_E_ID.3")) name = "VOLKSWAGEN_ID.3";
+                    if (name.equals("PEUGEOT_E_ION")) name = "PEUGEOT_ION";
+                    if (name.equals("FIAT_500")) name = "FIAT_500E";
+                    boolean carExists = false;
+                    for (CarData car : cars) {
+                        if (Objects.equals(car.name, name)) {
+                            car.count += count;
+                            carExists = true;
+                            break;
+                        }
+                    }
+                    if (!carExists) {
+                        CarData car = new CarData();
+                        car.name = name;
+                        car.count = count;
+                        cars.add(car);
+                    }
                 }
             }
             index++;
         }
 
-        // Get car info from https://pod-point.com/guides/vehicles/ with celenium
+        // Get car info from https://pod-point.com/guides/vehicles/ with selenium
         WebDriver driver = new FirefoxDriver();
         try {
+            // Find links for cars
             driver.get("https://pod-point.com/guides/vehicles/");
             List<WebElement> elements = driver.findElements(By.cssSelector("div.selectable-vehicle-card"));
             for (WebElement element : elements) {
-                WebElement typeElement = element.findElement(By.cssSelector("div:nth-child(1) > a:nth-child(1) > div:nth-child(3) > ul:nth-child(2) > li:nth-child(1)"));
-                System.out.println(typeElement.getText());
+                String[] elementData = element.getText().split("\n");
+                String carName = elementData[0].replaceAll("-", "_").replaceAll(" ", "_").replaceAll("!", "1").replaceAll("_E_", "_").toUpperCase();
+                carName = Normalizer.normalize(carName, Normalizer.Form.NFKD).replaceAll("[^\\p{ASCII}]", "");
+                for (CarData carData : cars) {
+                    if (carData.link != null)
+                        continue;
+                    if (carName.contains(carData.name)) {
+                        carData.link = element.findElement(By.cssSelector("a")).getAttribute("href");
+                        break;
+                    }
+                }
+            }
+
+            // Go through all found links and get data
+            for (CarData carData : cars) {
+                if (carData.link == null) {
+                    System.out.println("No link found for "+carData.name);
+                    continue;
+                }
+                driver.get(carData.link);
+
+                WebElement carNameContainer = driver.findElement(By.cssSelector(".p-v-xl > div:nth-child(1) > div:nth-child(1)"));
+                String carName = carNameContainer.findElement(By.cssSelector("h1")).getText();
+                System.out.println("Link found for "+carData.name+": "+carName);
+
+                // ZENDIUM
+
             }
         } finally {
             driver.quit();
